5b52870095f73a9d74036925cf62e41ed5bda6fd
Cyber-Jollof <53369822+Cyb3r-J0ll0f@users.noreply.github.com>
2021-09-17 13:34:56 +0100

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,33 @@
+# Python Variables - Example Program
+
+num1=10
+num2=20
+sum=num1+num2
+
+print(sum)
+
+#Let's take another example on variable in Python. In this example, program will ask to the user to enter some detail to fetch the input and output accordingly as shown below.
+
+variable_input = input("Who are you ? ")
+print("Hello, "+variable_input)
+input_variable = input("What is your name ? ")
+print("You are "+input_variable)
+var = input("Why are you here ? ")
+print("You are here to "+var)
+
+
+#Here is an example program, uses three variables in python namely m, n, and r
+
+m=54
+n=45
+r=m+n
+print("sum = ", r)
+r=m-n
+print("Subtract = ", r)
+r=m*n
+print("Multiply = ", r)
+r=m/n
+print("Divide = ", r)
+
+
+
